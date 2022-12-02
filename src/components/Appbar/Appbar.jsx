b9a10117902231f7a364cba3458767f629a64045
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-02 23:51:53 +0200

    add fix

--- src/components/Appbar/Appbar.jsx
+++ src/components/Appbar/Appbar.jsx
@@ -2,7 +2,7 @@ import { Box } from '../Box';
 import { StyledLink } from './Appbar.styled';
 
 const navItems = [
-  { href: 'home', text: 'Home' },
+  { href: '/', text: 'Home' },
   { href: 'movies', text: 'Movies' },
 ];
 
