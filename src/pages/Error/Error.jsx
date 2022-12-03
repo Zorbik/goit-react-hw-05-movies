d60d48986c7e6498958a256edf7eb1d2986bc5e7
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-03 12:15:05 +0200

    fix

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import { Box } from '../../components';
+
+const Error = () => {
+  return (
+    <Box>
+      <h1>Такої сторінки, нажаль, немає</h1>
+    </Box>
+  );
+};
+
+export default Error;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 // import { lazy } from 'react';
-// // export { default as Movies } from './MoviesPage/Movies';
-// // export { default as Reviews } from './MovieDetailsPage/Reviews';
-// // export { default as MovieDetails } from './MovieDetailsPage/MovieDetails';
-// // export { default as Cast } from './MovieDetailsPage/Cast';
-// export { default as Home } from './HomePage/HomePage';
-// // export const Home = lazy(() => import('./HomePage/HomePage'));
+export { default as Movies } from './MoviesPage/Movies';
+export { default as Reviews } from './MovieDetailsPage/Reviews';
+export { default as MovieDetails } from './MovieDetailsPage/MovieDetails';
+export { default as Cast } from './MovieDetailsPage/Cast';
+export { default as Home } from './HomePage/HomePage';
+export { default as Error } from './Error/Error';
+
+// export const Home = lazy(() => import('./HomePage/HomePage'));
 
 // export const Movies = lazy(() => import('./MoviesPage/Movies'));
 // export const Reviews = lazy(() => import('./MovieDetailsPage/Reviews'));
