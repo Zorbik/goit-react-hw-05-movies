e409854610c02c5394b74b2e7dcb313c1b713fa7
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-03 14:01:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import { App } from './App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/goit-react-hw-05-movies">
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
         <App />
       </BrowserRouter>
     </ThemeProvider>
