93404467ece9f14fc1657c89a3908eea9eddaf27
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-03 16:21:24 +0200

    sss

--- src/components/Home/ListItem.jsx
+++ src/components/Home/ListItem.jsx
@@ -1,27 +1,20 @@
 import { baseImageUrl } from '../../services/fetchAPI';
-import {
-  StyledItem,
-  StyledLink,
-  StyledImg,
-  TitleMovie,
-} from './ListItem.styled';
+import { StyledImg, TitleMovie } from './ListItem.styled';
 
 export const ListItem = ({ items, state }) => {
-  const { id, backdrop_path, title, name } = items;
+  const { backdrop_path, title, name } = items;
   return (
-    <StyledItem>
-      <StyledLink to={`/movies/${id}`} state={state}>
-        <StyledImg
-          src={
-            backdrop_path
-              ? baseImageUrl + backdrop_path
-              : 'http://dummyimage.com/500x280/c0c0c0.jpg&text=Картинку вкрали кацапи :('
-          }
-          alt={title ?? name}
-          width="500"
-        />
-        <TitleMovie>{title ?? name}</TitleMovie>
-      </StyledLink>
-    </StyledItem>
+    <>
+      <StyledImg
+        src={
+          backdrop_path
+            ? baseImageUrl + backdrop_path
+            : 'http://dummyimage.com/500x280/c0c0c0.jpg&text=Картинку вкрали кацапи :('
+        }
+        alt={title ?? name}
+        width="500"
+      />
+      <TitleMovie>{title ?? name}</TitleMovie>
+    </>
   );
 };
--- src/components/Movies/GalleryItems.jsx
+++ src/components/Movies/GalleryItems.jsx
@@ -1,12 +1,17 @@
 import { Box, ListItem } from '../../components';
 import { useLocation } from 'react-router-dom';
+import { StyledItem, StyledLink } from '../Home/ListItem.styled';
 
 export const GalleryItems = ({ movies }) => {
   const location = useLocation();
   return (
     <Box as="ul" display="flex" flexWrap="wrap" justifyContent="center" p={0}>
       {movies?.map(movie => (
-        <ListItem key={movie.id} items={movie} state={{ from: location }} />
+        <StyledItem key={movie.id}>
+          <StyledLink to={`/movies/${movie.id}`} state={{ from: location }}>
+            <ListItem items={movie} />
+          </StyledLink>
+        </StyledItem>
       ))}
     </Box>
   );
