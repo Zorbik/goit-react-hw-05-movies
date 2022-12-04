8060f375dc934d57f79c5962ffff437bca9dfbaf
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-04 13:14:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,18 @@ const Error = lazy(() => import('./pages/Error/Error'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Error />} />
         </Route>
-        <Route path="*" element={<Error />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 };
