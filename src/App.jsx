0739e7e5edb9552917a678d9717e97acb111c772
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-04 13:10:19 +0200

    ввв

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { Layout } from './components';
 
 const Home = lazy(() => import('./pages/HomePage/HomePage'));
 const Movies = lazy(() => import('./pages/MoviesPage/Movies'));
-// const Reviews = lazy(() => import('./pages/MovieDetailsPage/Reviews'));
-// const MovieDetails = lazy(() =>
-//   import('./pages/MovieDetailsPage/MovieDetails')
-// );
-// const Cast = lazy(() => import('./pages/MovieDetailsPage/Cast'));
-// const Error = lazy(() => import('./pages/Error/Error'));
+const Reviews = lazy(() => import('./pages/MovieDetailsPage/Reviews'));
+const MovieDetails = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetails')
+);
+const Cast = lazy(() => import('./pages/MovieDetailsPage/Cast'));
+const Error = lazy(() => import('./pages/Error/Error'));
 
 export const App = () => {
   return (
@@ -17,11 +17,11 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
-        {/* <Route path="movies/:movieId" element={<MovieDetails />}>
+        <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<Error />} /> */}
+        <Route path="*" element={<Error />} />
       </Route>
     </Routes>
   );
