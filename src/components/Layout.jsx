2ed5db7c6d0114a1a30245b915ab5dc896032ad9
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2022-12-04 21:13:38 +0200

    add update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import { ProgressBar } from 'react-loader-spinner';
-import { Box, Appbar } from '../components';
+import { Box, Appbar, loader } from '../components';
 
 export const Layout = () => {
   return (
     <Box>
       <Appbar></Appbar>
-      <Suspense
-        fallback={
-          <ProgressBar
-            height="80"
-            width="80"
-            ariaLabel="progress-bar-loading"
-            wrapperStyle={{}}
-            wrapperClass="progress-bar-wrapper"
-            borderColor="#F4442E"
-            barColor="#51E5FF"
-          />
-        }
-      >
+      <Suspense fallback={loader}>
         <Outlet />
       </Suspense>
     </Box>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import { ProgressBar } from 'react-loader-spinner';
+
 export { Appbar } from './Appbar/Appbar';
 export { Box } from './Box';
 export { ListItem } from './Home/ListItem';
@@ -9,3 +11,15 @@ export { MovieItem } from './Movies/MovieItem';
 export { ReviewsItem } from './Movies/Reviews/ReviewsItem';
 export { ReviewsList } from './Movies/Reviews/ReviewsList';
 export { SearchBox } from './SearchBox/SearchBox';
+
+export const loader = (
+  <ProgressBar
+    height="80"
+    width="80"
+    ariaLabel="progress-bar-loading"
+    wrapperStyle={{}}
+    wrapperClass="progress-bar-wrapper"
+    borderColor="#F4442E"
+    barColor="#51E5FF"
+  />
+);
